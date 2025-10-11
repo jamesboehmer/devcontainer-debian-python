c06f47d081979b32b8a2528fe9e66d8c9517f1f3
James Boehmer <750457+jamesboehmer@users.noreply.github.com>
2025-10-11 18:19:45 +0000

    Removed all unnecessary apt installations

--- .devcontainer/dockerfiles/install-apt-packages.sh
+++ .devcontainer/dockerfiles/install-apt-packages.sh
@@ -2,22 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update && \
-apt-get -y install \
-postgresql \
-default-jdk \
-openvpn3 \
-tailscale \
-google-cloud-cli \
-gh \
-jq \
-xdg-utils
-
-# Auth0 CLI
-curl -sSfL https://raw.githubusercontent.com/auth0/auth0-cli/main/install.sh | sh -s -- -b . && mv ./auth0 /usr/local/bin/
-
-# ArgoCD CLI
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64;
-install -m 555 argocd-linux-amd64 /usr/local/bin/argocd;
-rm argocd-linux-amd64;
-
+apt-get update && apt-get -y install xdg-utils # enables opening URLs automatically in vscode devcontainer
